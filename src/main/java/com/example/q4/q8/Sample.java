7e7f10346b8aa2ceb3012d5134717e625206cc7f
tktktakatuska <blue.mountain.syota@icloud.com>
2024-08-04 11:51:06 +0900

    hoge

--- src/main/java/com/example/q4/q8/Sample.java
+++ src/main/java/com/example/q4/q8/Sample.java
@@ -5,15 +5,28 @@ import java.util.Optional;
 public class Sample {
     public static void main(String[] args) {
         Optional<Integer> a = Optional.of(100);
-        Optional<Integer> b = a.flatMap(price -> {
+
+        // オプショナルクラスのマップmethodを利用
+        Optional<Integer> b = a.map(price -> {
             return calc(price, 3);
         });
 
+        // オプショナルを戻す場合は、flatmapメソッドを利用。
+        Optional<Integer> c = a.flatMap(price -> {
+            return optionalCalc(price, 3);
+        });
+
         System.out.println(b.get());
+        System.out.println(c.get());
     }
 
+    // 値を戻すメソッド→ mapメソッドを利用
+    private static Integer calc(int price, int qty) {
+        return price * qty;
+    }
 
-    private static Optional<Integer> calc(int price, int qty){
+    // オプショナルクラスを戻すメソッド→ フラットマップメソッドを利用
+    private static Optional<Integer> optionalCalc(int price, int qty) {
         return Optional.of(price * qty);
     }
 }
